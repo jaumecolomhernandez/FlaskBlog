6e5d5b9734dc2ffd3883d87397db2f854ef21c5c
jc <jaumecolomhernandez@gmail.com>
2019-02-12 23:36:05 +0100

    Added email  plugin and reset page

--- app/__init__.py
+++ app/__init__.py
@@ -3,11 +3,13 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
+
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 #migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+mail = Mail(app)
 
-from app import routes
+from app import routes, models, errors
